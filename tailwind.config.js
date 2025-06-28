b2dc15c8709c9761fe81940455d711e897868077
rahulmatthan <65018669+rahulmatthan@users.noreply.github.com>
2025-06-28 12:32:30 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ['Georgia', 'Times New Roman', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
